b0ddc34d559f536169922d2171eb281610aec3c8
Meghani M. Mehboob <suitable9004@gmail.com>
2021-06-20 02:54:26 +0530

    Fixed Header Position, Added Categories

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 
 import db
 
 app = Flask(__name__)
+data = ""
 
 @app.route('/')
 def home():
-    return redirect(url_for("news"))
+    global data
+    return render_template("index.html", datas = data)
     # return 'News API is UP!<br><br>A part of <a href="https://t.me/sjprojects">Sj Projects</a>'
 
 @app.route('/news')
 def news():
-    data = db.readsheet()[0].to_records()
-    return render_template("index.html", datas = data)
+    global data
+    data = db.readsheet()[0]
+    reqcat = request.args.get("cat")
+    if not reqcat == None:
+        data = data.loc[data["Category"]==reqcat]
+    data = data.to_records()
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.debug = True
