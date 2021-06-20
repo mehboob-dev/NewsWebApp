3ad5542d6a61cf01f6541f7b536c105718c123fd
Meghani M. Mehboob <suitable9004@gmail.com>
2021-06-20 17:11:57 +0530

    Fix app.py file Formatting

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ import db
 app = Flask(__name__)
 data = db.readsheet()[0].to_records()
 
+
 @app.route('/')
 def home():
     global data
-    return render_template("index.html", datas = data)
+    return render_template("index.html", datas=data)
     # return 'News API is UP!<br><br>A part of <a href="https://t.me/sjprojects">Sj Projects</a>'
+
 
 @app.route('/news')
 def news():
     global data
     data = db.readsheet()[0]
     reqcat = request.args.get("cat")
-    if not reqcat == None:
-        data = data.loc[data["Category"]==reqcat]
+    if reqcat is not None:
+        data = data.loc[data["Category"] == reqcat]
     data = data.to_records()
     return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.debug = True
